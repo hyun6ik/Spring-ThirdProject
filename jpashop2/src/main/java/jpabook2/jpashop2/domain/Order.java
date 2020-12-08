94e01efb758a56fc49e0f9485b0a419bad89cbfe
hyun6ik <fkanwm@naver.com>
2020-12-08 23:27:07 +0900

    주문 Service 개발

--- jpashop2/src/main/java/jpabook2/jpashop2/domain/Order.java
+++ jpashop2/src/main/java/jpabook2/jpashop2/domain/Order.java
@@ -1,6 +1,8 @@
 package jpabook2.jpashop2.domain;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -14,6 +16,7 @@ import static javax.persistence.FetchType.*;
 @Table(name = "orders")
 @Getter
 @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Order {
     @Id
     @GeneratedValue
@@ -37,7 +40,7 @@ public class Order {
     private OrderStatus status;  //주문상태 [ORDER, CANCEL]
 
     //연관관계 메서드
-    public void setmember(Member member){
+    public void setMember(Member member){
         this.member = member;
         member.getOrders().add(this);
     }
@@ -56,7 +59,7 @@ public class Order {
     //==생성 메서드==//
     public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems){
         Order order = new Order();
-        order.setmember(member);
+        order.setMember(member);
         order.setDelivery(delivery);
         for (OrderItem orderItem : orderItems) {
             order.addOrderItem(orderItem);
