55c98dd131d9f496ce4e2027a268dae42fc8998f
hyun6ik <fkanwm@naver.com>
2020-12-08 19:49:36 +0900

    H2 데이터베이스 설치, 도메인 모델, 테이블 설계, 엔티티 클래스 개발

---
+++ jpashop2/src/main/java/jpabook2/jpashop2/Hello.java
@@ -0,0 +1,11 @@
+package jpabook2.jpashop2;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Hello {
+
+    private String Data;
+}
---
+++ jpashop2/src/main/java/jpabook2/jpashop2/MemberRepository.java
@@ -0,0 +1,23 @@
+package jpabook2.jpashop2;
+
+import jpabook2.jpashop2.domain.Member;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public Long save(Member member){
+        em.persist(member);
+        return member.getId();
+    }
+
+    public Member find(Long id){
+        return em.find(Member.class, id);
+    }
+}
